bc58c2bfc7ad232f925b2f746b593f633ae28f05
lethamburn <@antoniorosalesmartinez90@gmail.com>
2021-08-16 19:54:27 +0200

    FEAT dashborad function and ts with service

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,16 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { ClimaService } from 'src/app/services/clima.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  urlImagen= "https://img.icons8.com/clouds/2x/apple-weather.png";
+  urlImagen = 'https://image.flaticon.com/icons/png/512/1116/1116453.png';
+  ciudad = '';
+  temperatura = 0;
+  humedad = 0;
+  clima = '';
+  query = false;
+  loading = false;
+  mostrarError = false;
 
-  constructor() { }
+  constructor(private _climaService: ClimaService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  obtenerClima() {
+    this.query = false;
+    this.loading = true;
+
+    this._climaService.getClima(this.ciudad).subscribe(
+      (data) => {
+        this.loading = false;
+        this.query = true;
+        this.temperatura = data.main.temp - 273;
+        this.humedad = data.main.humidity;
+        this.clima = data.weather[0].main;
+      },
+      (error) => {
+        console.log(error);
+        this.loading = false;
+        this.error();
+      }
+    );
   }
 
+  error() {
+    this.mostrarError = true;
+    setTimeout(() => {
+      this.mostrarError = false;
+      this.ciudad = '';
+    }, 3000);
+  }
 }
--- src/app/services/clima.service.ts
+++ src/app/services/clima.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClimaService {
+  url = 'https://api.openweathermap.org/data/2.5/weather?&appid=';
 
-  constructor() { }
+  key = 'bb3bd1023f471eba03a73e03b590bbd1';
+
+  constructor(private http: HttpClient) {}
+
+  getClima(ciudad: string): Observable<any> {
+    const URL = this.url + this.key + '&q=' + ciudad;
+    return this.http.get(URL);
+  }
 }
